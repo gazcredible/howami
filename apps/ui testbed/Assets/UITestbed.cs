cf8f4ae035a0811a8a042fd447c12ee744a001dc
Gareth <gareth_lewis@yahoo.com>
2020-01-30 15:56:55 +0000

    .

---
+++ apps/ui testbed/Assets/UITestbed.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITestbed : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Color textHighlight;
+
+    public bool hamburgerMenuActive;
+    public String mode;
+
+    void Start()
+    {
+        hamburgerMenuActive = false;
+        mode = "splash";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(mode)
+        {
+            case "splash":
+                break;
+
+            case "new_response":
+                break;
+
+            case "review_current":
+                break;
+
+            case "review_historic":
+                break;
+
+            case "support":
+                break;
+
+            default:
+                Debug.LogError(mode + " not supported");
+                return;
+        }
+    }
+
+    public void OnHamburgerToggle()
+    {
+        hamburgerMenuActive = !hamburgerMenuActive;
+    }
+
+    public void OnHamburgerSelect(String option)
+    {
+        if(option != mode)
+        {
+            //do something
+            mode = option;
+        }      
+    }
+}
