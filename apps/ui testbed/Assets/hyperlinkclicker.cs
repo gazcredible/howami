69c7f823d571f9737469c46dd6ba3631aeb75d58
Gareth Lewis <gareth_lewis@yahoo.com>
2020-07-26 20:35:53 +0100

    add hyperlinks

---
+++ apps/ui testbed/Assets/hyperlinkclicker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hyperlinkclicker : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string label;
+    public string hyperlink;
+
+    void Start()
+    {
+        transform.Find("Text").GetComponent<UnityEngine.UI.Text>().text = label;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onclick()
+    {
+        Application.OpenURL(hyperlink);
+    }
+}
